8c01c6f0756764304d9164a13eb1b24bddff68f5
Andrew Morris <andrewmamorris@gmail.com>
2023-03-29 12:32:47 +0200

    before lunch

--- crypto_prem.py
+++ crypto_prem.py
@@ -1,7 +1,13 @@
 #definitions variables
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+index = 0
 #input mot et indice
+mot_a_cacher = input("Secret word: ")
+decalage = input("Key")
 
 #opérations:
 #boucle for qui parcourt le mot
-#if in alph
+for
+#if in alph: alph+= pour incrémenter
+if c in alphabet:
+    alphabet[index] += ((26 + ind) % 26)
